84fc059a1b1e289a1dbc1343e347b5f3a481b830
sajaalmuraqtn <sajaalmuraqtn@gmail.com>
2024-08-13 05:53:14 +0300

    last edit

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
-import express from 'express';
-import dotenv from 'dotenv';
+import express from 'express'
+import dotenv from 'dotenv'
 import initApp from './Src/Modules/app.router.js';
+dotenv.config()
+const app=express()
 
-dotenv.config();
-const app = express();
+const PORT=process.env.PORT || 3000;
 
-initApp(app, express);
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+app.listen(PORT,()=>{
+    console.log(`server is running on port ${PORT} `);
+})
+initApp(app,express);
